9780cd798df95c50d7519616f65da1c13f64759f
Angelina-Pysanyuk <110035505+Angelina-Pysanyuk@users.noreply.github.com>
2023-01-16 10:15:14 +0000

    Deploying to gh-pages from @ Angelina-Pysanyuk/goit-js-hw-09@7cb4768e3496d9cdfb5d4bf15537af294f8bba42 🚀

---
+++ 01-color-switcher.777a107b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");let a=null;d.disabled=!0;t.addEventListener("click",(()=>{a=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;e.style.backgroundColor=t}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.777a107b.js.map
